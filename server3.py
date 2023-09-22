b30fc69e32e50eb9c36fa979fef806660fd7a7e8
Eric Pan <75194970+EricPanDev@users.noreply.github.com>
2023-09-22 15:37:33 -0700

    Update server3.py

--- server3.py
+++ server3.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, redirect
 import user_agent
 
 URL_1 = "https://github.com/EricPanDev/SillyRepository/blob/main/bait.png"
